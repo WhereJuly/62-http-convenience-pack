bd3a34830c05e2aee91d48e74e5981e409b4d8f0
WhereJuly <where.july@googlemail.com>
2024-11-26 13:13:28 +0300

    [methods] Update (package, usage): export as namespace, new use cases

--- .usage/tests/index.ts
+++ .usage/tests/index.ts
@@ -1,7 +1,8 @@
 'use strict';
 
-import { EHTTPMethods } from '@wherejuly/http-convenience-pack';
+import { EHTTPMethods, HCP } from '@wherejuly/http-convenience-pack';
 
 const method = EHTTPMethods.GET;
+const method2 = HCP.EMethods.GET;
 
-console.log(method);
+console.log(method, method2);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 'use strict';
 
+import {
+    EHTTPMethods as _EHTTPMethods,
+    default as _HTTPMethodsConvenience,
+    type TCustomHTTPMethodsConstraint as _TCustomHTTPMethodsConstraint
+} from './core/methods/HTTPMethodsConvenience.js';
+
+
+export namespace HCP {
+    export import EMethods = _EHTTPMethods;
+    export type TMethodsConstraint = _TCustomHTTPMethodsConstraint;
+    export class Methods extends _HTTPMethodsConvenience { };
+}
+
 export { EHTTPMethods, default as HTTPMethodsConvenience, type TCustomHTTPMethodsConstraint } from './core/methods/HTTPMethodsConvenience.js';
