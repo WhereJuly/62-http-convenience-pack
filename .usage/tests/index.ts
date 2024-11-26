51ab6c81c7fe2995a7785f854769826ea9e15cdf
WhereJuly <where.july@googlemail.com>
2024-11-26 10:12:05 +0300

    [pack] Update (icicdr): rewrite from clean state to avoid eslint any type issue

---
+++ .usage/tests/index.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+import { EHTTPMethods } from '@wherejuly/http-convenience-pack';
+
+const method = EHTTPMethods.GET;
+
+console.log(method);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 'use strict';
 
-import {
-    EHTTPMethods as _EHTTPMethods,
-    default as _HTTPMethodsConvenience,
-    type TCustomHTTPMethodsConstraint as _TCustomHTTPMethodsConstraint
-} from './core/methods/HTTPMethodsConvenience.js';
-
-export namespace HTTPConveniencePack {
-    export import EHTTPMethods = _EHTTPMethods;
-    export type TCustomHTTPMethodsConstraint = _TCustomHTTPMethodsConstraint;
-    export class HTTPMethodsConvenience extends _HTTPMethodsConvenience { };
-}
+export { EHTTPMethods, default as HTTPMethodsConvenience, type TCustomHTTPMethodsConstraint } from './core/methods/HTTPMethodsConvenience.js';
