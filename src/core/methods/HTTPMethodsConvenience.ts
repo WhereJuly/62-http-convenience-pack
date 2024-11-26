03281ed405cd0b3db2f63603393d86582edb53e6
WhereJuly <where.july@googlemail.com>
2024-11-26 14:25:37 +0300

    [methods] Update (docs): doc block

--- src/core/methods/HTTPMethodsConvenience.ts
+++ src/core/methods/HTTPMethodsConvenience.ts
@@ -8,6 +8,7 @@ export type TCustomHTTPMethodsConstraint = Record<string, string>;
  * The standard HTTP methods enum.
  * 
  * Comply with [RFC 9110](https://www.rfc-editor.org/rfc/rfc9110#methods)
+ * published in June 2022.
  */
 export enum EHTTPMethods {
     GET = 'GET',
