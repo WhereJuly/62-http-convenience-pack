e3ed419cbd590018a7220f96628fa990a5161527
WhereJuly <where.july@googlemail.com>
2024-11-27 14:12:50 +0300

    [mime] Cleanup (module): format

--- src/core/mime/types/popular.mime.ts
+++ src/core/mime/types/popular.mime.ts
@@ -3,6 +3,8 @@
 import { TMIMETypeEntry } from '../utility.types.js';
 import { EMIMEExtensions, EMIMEGroups } from './common.mime.js';
 
+// WRITE: Grouped popular MIME types
+
 export enum EPopularMIMETypes {
     // Text MIME Types
     TEXT_XML = "text/xml",
@@ -59,163 +61,99 @@ export const MIME_TYPES_POPULAR: {
     [key in EPopularMIMETypes]: TMIMETypeEntry<EPopularMIMETypes>;
 } = {
     [EPopularMIMETypes.TEXT_XML]: {
-        type: EPopularMIMETypes.TEXT_XML,
-        extension: EMIMEExtensions.TEXT_XML,
-        group: EMIMEGroups.TEXT,
+        type: EPopularMIMETypes.TEXT_XML, extension: EMIMEExtensions.TEXT_XML, group: EMIMEGroups.TEXT,
     },
     [EPopularMIMETypes.TEXT_YAML]: {
-        type: EPopularMIMETypes.TEXT_YAML,
-        extension: EMIMEExtensions.TEXT_YAML,
-        group: EMIMEGroups.TEXT,
+        type: EPopularMIMETypes.TEXT_YAML, extension: EMIMEExtensions.TEXT_YAML, group: EMIMEGroups.TEXT,
     },
     [EPopularMIMETypes.TEXT_MARKDOWN]: {
-        type: EPopularMIMETypes.TEXT_MARKDOWN,
-        extension: EMIMEExtensions.TEXT_MARKDOWN,
-        group: EMIMEGroups.TEXT,
+        type: EPopularMIMETypes.TEXT_MARKDOWN, extension: EMIMEExtensions.TEXT_MARKDOWN, group: EMIMEGroups.TEXT,
     },
     [EPopularMIMETypes.TEXT_RICHTEXT]: {
-        type: EPopularMIMETypes.TEXT_RICHTEXT,
-        extension: EMIMEExtensions.TEXT_RICHTEXT,
-        group: EMIMEGroups.TEXT,
+        type: EPopularMIMETypes.TEXT_RICHTEXT, extension: EMIMEExtensions.TEXT_RICHTEXT, group: EMIMEGroups.TEXT,
     },
     [EPopularMIMETypes.IMAGE_BMP]: {
-        type: EPopularMIMETypes.IMAGE_BMP,
-        extension: EMIMEExtensions.IMAGE_BMP,
-        group: EMIMEGroups.IMAGE,
+        type: EPopularMIMETypes.IMAGE_BMP, extension: EMIMEExtensions.IMAGE_BMP, group: EMIMEGroups.IMAGE,
     },
     [EPopularMIMETypes.IMAGE_SVG_XML]: {
-        type: EPopularMIMETypes.IMAGE_SVG_XML,
-        extension: EMIMEExtensions.IMAGE_SVG_XML,
-        group: EMIMEGroups.IMAGE,
+        type: EPopularMIMETypes.IMAGE_SVG_XML, extension: EMIMEExtensions.IMAGE_SVG_XML, group: EMIMEGroups.IMAGE,
     },
     [EPopularMIMETypes.IMAGE_WEBP]: {
-        type: EPopularMIMETypes.IMAGE_WEBP,
-        extension: EMIMEExtensions.IMAGE_WEBP,
-        group: EMIMEGroups.IMAGE,
+        type: EPopularMIMETypes.IMAGE_WEBP, extension: EMIMEExtensions.IMAGE_WEBP, group: EMIMEGroups.IMAGE,
     },
     [EPopularMIMETypes.IMAGE_HEIF]: {
-        type: EPopularMIMETypes.IMAGE_HEIF,
-        extension: EMIMEExtensions.IMAGE_HEIF,
-        group: EMIMEGroups.IMAGE,
+        type: EPopularMIMETypes.IMAGE_HEIF, extension: EMIMEExtensions.IMAGE_HEIF, group: EMIMEGroups.IMAGE,
     },
     [EPopularMIMETypes.VIDEO_WEBM]: {
-        type: EPopularMIMETypes.VIDEO_WEBM,
-        extension: EMIMEExtensions.VIDEO_WEBM,
-        group: EMIMEGroups.VIDEO,
+        type: EPopularMIMETypes.VIDEO_WEBM, extension: EMIMEExtensions.VIDEO_WEBM, group: EMIMEGroups.VIDEO,
     },
     [EPopularMIMETypes.VIDEO_OGG]: {
-        type: EPopularMIMETypes.VIDEO_OGG,
-        extension: EMIMEExtensions.VIDEO_OGG,
-        group: EMIMEGroups.VIDEO,
+        type: EPopularMIMETypes.VIDEO_OGG, extension: EMIMEExtensions.VIDEO_OGG, group: EMIMEGroups.VIDEO,
     },
     [EPopularMIMETypes.VIDEO_AVI]: {
-        type: EPopularMIMETypes.VIDEO_AVI,
-        extension: EMIMEExtensions.VIDEO_AVI,
-        group: EMIMEGroups.VIDEO,
+        type: EPopularMIMETypes.VIDEO_AVI, extension: EMIMEExtensions.VIDEO_AVI, group: EMIMEGroups.VIDEO,
     },
     [EPopularMIMETypes.VIDEO_3GP]: {
-        type: EPopularMIMETypes.VIDEO_3GP,
-        extension: EMIMEExtensions.VIDEO_3GP,
-        group: EMIMEGroups.VIDEO,
+        type: EPopularMIMETypes.VIDEO_3GP, extension: EMIMEExtensions.VIDEO_3GP, group: EMIMEGroups.VIDEO,
     },
     [EPopularMIMETypes.AUDIO_WAV]: {
-        type: EPopularMIMETypes.AUDIO_WAV,
-        extension: EMIMEExtensions.AUDIO_WAV,
-        group: EMIMEGroups.AUDIO,
+        type: EPopularMIMETypes.AUDIO_WAV, extension: EMIMEExtensions.AUDIO_WAV, group: EMIMEGroups.AUDIO,
     },
     [EPopularMIMETypes.AUDIO_OGG]: {
-        type: EPopularMIMETypes.AUDIO_OGG,
-        extension: EMIMEExtensions.AUDIO_OGG,
-        group: EMIMEGroups.AUDIO,
+        type: EPopularMIMETypes.AUDIO_OGG, extension: EMIMEExtensions.AUDIO_OGG, group: EMIMEGroups.AUDIO,
     },
     [EPopularMIMETypes.AUDIO_FLAC]: {
-        type: EPopularMIMETypes.AUDIO_FLAC,
-        extension: EMIMEExtensions.AUDIO_FLAC,
-        group: EMIMEGroups.AUDIO,
+        type: EPopularMIMETypes.AUDIO_FLAC, extension: EMIMEExtensions.AUDIO_FLAC, group: EMIMEGroups.AUDIO,
     },
     [EPopularMIMETypes.AUDIO_WEBM]: {
-        type: EPopularMIMETypes.AUDIO_WEBM,
-        extension: EMIMEExtensions.AUDIO_WEBM,
-        group: EMIMEGroups.AUDIO,
+        type: EPopularMIMETypes.AUDIO_WEBM, extension: EMIMEExtensions.AUDIO_WEBM, group: EMIMEGroups.AUDIO,
     },
     [EPopularMIMETypes.APPLICATION_PDF]: {
-        type: EPopularMIMETypes.APPLICATION_PDF,
-        extension: EMIMEExtensions.APPLICATION_PDF,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_PDF, extension: EMIMEExtensions.APPLICATION_PDF, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.APPLICATION_ZIP]: {
-        type: EPopularMIMETypes.APPLICATION_ZIP,
-        extension: EMIMEExtensions.APPLICATION_ZIP,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_ZIP, extension: EMIMEExtensions.APPLICATION_ZIP, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.APPLICATION_GZIP]: {
-        type: EPopularMIMETypes.APPLICATION_GZIP,
-        extension: EMIMEExtensions.APPLICATION_GZIP,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_GZIP, extension: EMIMEExtensions.APPLICATION_GZIP, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.APPLICATION_TAR]: {
-        type: EPopularMIMETypes.APPLICATION_TAR,
-        extension: EMIMEExtensions.APPLICATION_TAR,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_TAR, extension: EMIMEExtensions.APPLICATION_TAR, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.APPLICATION_JAR]: {
-        type: EPopularMIMETypes.APPLICATION_JAR,
-        extension: EMIMEExtensions.APPLICATION_JAR,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_JAR, extension: EMIMEExtensions.APPLICATION_JAR, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.APPLICATION_XHTML_XML]: {
-        type: EPopularMIMETypes.APPLICATION_XHTML_XML,
-        extension: EMIMEExtensions.APPLICATION_XHTML_XML,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_XHTML_XML, extension: EMIMEExtensions.APPLICATION_XHTML_XML, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.MULTIPART_MIXED]: {
-        type: EPopularMIMETypes.MULTIPART_MIXED,
-        extension: EMIMEExtensions.MULTIPART_MIXED,
-        group: EMIMEGroups.MULTIPART,
+        type: EPopularMIMETypes.MULTIPART_MIXED, extension: EMIMEExtensions.MULTIPART_MIXED, group: EMIMEGroups.MULTIPART,
     },
     [EPopularMIMETypes.MULTIPART_ALTERNATIVE]: {
-        type: EPopularMIMETypes.MULTIPART_ALTERNATIVE,
-        extension: EMIMEExtensions.MULTIPART_ALTERNATIVE,
-        group: EMIMEGroups.MULTIPART,
+        type: EPopularMIMETypes.MULTIPART_ALTERNATIVE, extension: EMIMEExtensions.MULTIPART_ALTERNATIVE, group: EMIMEGroups.MULTIPART,
     },
     [EPopularMIMETypes.MULTIPART_RELATED]: {
-        type: EPopularMIMETypes.MULTIPART_RELATED,
-        extension: EMIMEExtensions.MULTIPART_RELATED,
-        group: EMIMEGroups.MULTIPART,
+        type: EPopularMIMETypes.MULTIPART_RELATED, extension: EMIMEExtensions.MULTIPART_RELATED, group: EMIMEGroups.MULTIPART,
     },
     [EPopularMIMETypes.FONT_TTF]: {
-        type: EPopularMIMETypes.FONT_TTF,
-        extension: EMIMEExtensions.FONT_TTF,
-        group: EMIMEGroups.FONT,
+        type: EPopularMIMETypes.FONT_TTF, extension: EMIMEExtensions.FONT_TTF, group: EMIMEGroups.FONT,
     },
     [EPopularMIMETypes.FONT_OTF]: {
-        type: EPopularMIMETypes.FONT_OTF,
-        extension: EMIMEExtensions.FONT_OTF,
-        group: EMIMEGroups.FONT,
+        type: EPopularMIMETypes.FONT_OTF, extension: EMIMEExtensions.FONT_OTF, group: EMIMEGroups.FONT,
     },
     [EPopularMIMETypes.FONT_WOFF]: {
-        type: EPopularMIMETypes.FONT_WOFF,
-        extension: EMIMEExtensions.FONT_WOFF,
-        group: EMIMEGroups.FONT,
+        type: EPopularMIMETypes.FONT_WOFF, extension: EMIMEExtensions.FONT_WOFF, group: EMIMEGroups.FONT,
     },
     [EPopularMIMETypes.FONT_WOFF2]: {
-        type: EPopularMIMETypes.FONT_WOFF2,
-        extension: EMIMEExtensions.FONT_WOFF2,
-        group: EMIMEGroups.FONT,
+        type: EPopularMIMETypes.FONT_WOFF2, extension: EMIMEExtensions.FONT_WOFF2, group: EMIMEGroups.FONT,
     },
     [EPopularMIMETypes.APPLICATION_SQL]: {
-        type: EPopularMIMETypes.APPLICATION_SQL,
-        extension: EMIMEExtensions.APPLICATION_SQL,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_SQL, extension: EMIMEExtensions.APPLICATION_SQL, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.APPLICATION_XSQL]: {
-        type: EPopularMIMETypes.APPLICATION_XSQL,
-        extension: EMIMEExtensions.APPLICATION_XSQL,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_XSQL, extension: EMIMEExtensions.APPLICATION_XSQL, group: EMIMEGroups.APPLICATION,
     },
     [EPopularMIMETypes.APPLICATION_JSONLD]: {
-        type: EPopularMIMETypes.APPLICATION_JSONLD,
-        extension: EMIMEExtensions.APPLICATION_JSONLD,
-        group: EMIMEGroups.APPLICATION,
+        type: EPopularMIMETypes.APPLICATION_JSONLD, extension: EMIMEExtensions.APPLICATION_JSONLD, group: EMIMEGroups.APPLICATION,
     },
 } as const;
