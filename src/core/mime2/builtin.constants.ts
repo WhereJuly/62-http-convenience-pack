d97296358f5f5a5613f04e54b46a41eb573742a1
WhereJuly <where.july@googlemail.com>
2024-11-28 14:15:52 +0300

    [mime] Implement (docs): comment code & docblocks

--- src/core/mime2/builtin.constants.ts
+++ src/core/mime2/builtin.constants.ts
@@ -4,10 +4,9 @@ import { MIMEExtensionsFactory, MIMEGroupsFactory, MIMETypesGenericRegistryFacto
 import { BuiltInMIMETypesSource } from '@src/core/mime2/source/builtin.mime.js';
 
 /**
- * NB: Including all the IANA MIME Types would still not remove the necessity for custom set of
- * MIME Types. Thus I will still have to implement a way to create extended sets with generics.
- * 
- * @see {@link TMIMETypeArray}
+ * The built-in MIME Types Registry `MIME_TYPES_BUILTIN` constant
+ * made from {@link BuiltInMIMETypesSource}  with the respective 
+ * `MIME_TYPES_GROUPS_BUILTIN` and `MIME_TYPES_EXTENSIONS_BUILTIN`.
  */
 export const MIME_TYPES_BUILTIN = MIMETypesGenericRegistryFactory<typeof BuiltInMIMETypesSource>(BuiltInMIMETypesSource);
 export const MIME_TYPES_GROUPS_BUILTIN = MIMEGroupsFactory(BuiltInMIMETypesSource);
