449d644622f724bde6459fda336bb789f23e3932
WhereJuly <where.july@googlemail.com>
2024-11-28 07:46:32 +0300

    [mime] Refactor (module): explore the typed single source of truth solution

--- src/core/mime/types/revised-approach.ts
+++ src/core/mime/types/revised-approach.ts
@@ -7,6 +7,8 @@ const types = [
 
 type MIMETypeArray = typeof types;
 
+// --- MIMEObject
+
 type MIMETypeObject<T extends [string, string, string]> = {
     type: T[0];
     group: T[1];
@@ -26,9 +28,43 @@ const MIME_TYPES = Object.fromEntries(
     ])
 ) as MIMERecord;
 
-// Now accessing 'group' should work correctly:
+// --- MIME Groups
+
+type MIMEGroupRecord<T extends readonly (readonly [string, string, string])[]> = {
+    [G in T[number][1]]: G; // Maps group keys to their own value
+};
+
+
+type MIMEGroup = MIMEGroupRecord<MIMETypeArray>;
+
+const MIME_GROUPS: MIMEGroup = Object.fromEntries(
+    types.map(([_, group]) => [group, group])
+) as MIMEGroup;
+
+// --- MIME Extensions
+
+type MIMEExtensionsRecord<T extends readonly (readonly [string, string, string])[]> = {
+    [E in T[number][2]]: E; // Maps extension keys to their own value
+};
+
+type MIMEExtensions = MIMEExtensionsRecord<MIMETypeArray>;
+
+const MIME_EXTENSIONS: MIMEExtensions = Object.fromEntries(
+    types.map(([_, __, extension]) => [extension, extension])
+) as MIMEExtensions;
+
+// --- Usage
+
+// MIME Types
 console.log(MIME_TYPES['application/json']);
 console.log(MIME_TYPES['application/json'].type); // Output: 'json'
+
+// MIME Groups
+console.log(MIME_GROUPS.txt); // Output: 'application/json'
+
+// MIME Extensions
+console.log(MIME_EXTENSIONS['.json']); // Output: '.json'
+console.log(MIME_EXTENSIONS['.txt']);  // Output: '.txt'
 
 // Explore: Get mime types grouped and selectable by group with autocomplete.
 // Explore: try to apply use cases (methods) from test file to this solution.
