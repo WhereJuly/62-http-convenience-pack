00f179716de678a06d303b86f58e599534ff597d
WhereJuly <where.july@googlemail.com>
2025-03-10 09:45:37 +0300

    [mime] Update (usage): demo

--- .usage/demo/autocomplete.ts
+++ .usage/demo/autocomplete.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { EHTTPHeaders, EHTTPMethods, EHTTPMethodsGroupsList, EMakerTokenSchemes, GROUPED_STATUS_CODES, HTTP_STATUSES, HTTPHeadersConvenience, HTTPMethodInGroups, HTTPMethodsConvenience, HTTPStatusesConvenience, MIME_TYPES_BUILTIN } from '@wherejuly/http-convenience-pack';
+import { ECacheControlServer, EHTTPHeaders, EHTTPMethods, EHTTPMethodsGroupsList, EMakerTokenSchemes, GROUPED_STATUS_CODES, HTTP_STATUSES, HTTPHeadersConvenience, HTTPMethodInGroups, HTTPMethodsConvenience, HTTPStatusesConvenience, MIME_TYPES_BUILTIN } from '@wherejuly/http-convenience-pack';
 
 /**
  * NB: Autocomplete Demo
@@ -21,7 +21,7 @@ console.log(GROUPED_STATUS_CODES.clienterr);
 
 HTTPStatusesConvenience.isAmong(success, [100]); // true
 
-const response = await fetch('https://api.example.com/data', {
+const _response = await fetch('https://api.example.com/data', {
     /**
      * Make or autocomplete the correct headers and mime types from the list provided.
      */
@@ -30,6 +30,8 @@ const response = await fetch('https://api.example.com/data', {
         ...HTTPHeadersConvenience.make(EHTTPHeaders.Authorization, EMakerTokenSchemes.Bearer, 'myBearerToken'),
 
         // Set no special treatment header with autocomplete using enums and constants
-        [EHTTPHeaders.ContentType]: MIME_TYPES_BUILTIN['image/png'].type
+        [EHTTPHeaders.Accept]: MIME_TYPES_BUILTIN['image/png'].type,
+        [EHTTPHeaders.ContentType]: MIME_TYPES_BUILTIN['text/event-stream'].type,
+        [EHTTPHeaders.CacheControl]: ECacheControlServer.NoCache
     }
 });
